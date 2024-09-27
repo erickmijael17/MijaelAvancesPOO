4ddf6376c84091f38a3c614fce6028d75a2afa48
erickmijael17 <erickmijaeldelgafolima@gmail.com>
2024-09-27 10:11:41 -0500

    actualizacion de base de datos e implementacion de id

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/dao/CalculadoraDao.java
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/dao/CalculadoraDao.java
@@ -22,6 +22,20 @@ public class CalculadoraDao {
         conn= con.getConn();
     }
 
+    public int idGenerate() {
+        int idx=0;
+        try {
+            ps=conn.prepareStatement("SELECT MAX((c.id)+1) AS id FROM calculadora c ;");
+            rs=ps.executeQuery();
+            if (rs.next()){
+                idx=rs.getInt("id");
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return idx ;
+    }
+
     public List<CalcTO> listar(){
         List<CalcTO> listC = new ArrayList<>();
         try {
@@ -29,6 +43,7 @@ public class CalculadoraDao {
             rs=ps.executeQuery();
             while(rs.next()){
                 CalcTO calcTO = new CalcTO();
+                calcTO.setId(rs.getInt("id"));
                 calcTO.setNum1(rs.getString("num1"));
                 calcTO.setNum2(rs.getString("num2"));
                 calcTO.setOperador(rs.getString("operador").charAt(0));
@@ -42,17 +57,47 @@ public class CalculadoraDao {
     }
 
     public void insertar(CalcTO calcTO){
+
+        int idx=idGenerate();
+        System.out.println(idx);
+        int i=0;
+        conn= con.getConn();
         try {
-            ps=conn.prepareStatement("INSERT into calculadora(num1, num2, operador, resultado) values(?, ?, ?, ?); ");
-            ps.setString(1, calcTO.getNum1());
-            ps.setString(2, calcTO.getNum2());
-            ps.setString(3, String.valueOf(calcTO.getOperador()));
-            ps.setString(4, calcTO.getResultado());
+            ps=conn.prepareStatement("INSERT into calculadora(id, num1, num2, operador, resultado) values(?,?, ?, ?, ?);  ");
+            ps.setInt(++i, idx);
+            ps.setString(++i, calcTO.getNum1());
+            ps.setString(++i, calcTO.getNum2());
+            ps.setString(++i, String.valueOf(calcTO.getOperador()));
+            ps.setString(++i, calcTO.getResultado());
+            ps.executeUpdate();
+            // System.out.println(idx+" n1:"+calcTO.getNum1()+" n2:"+calcTO.getNum2()+" op:"+calcTO.getOperador()+ calcTO.getResultado());
+        }catch (Exception e){
+            System.out.println("EIN:"+e.getMessage());
+        }
+
+    }
+    public void actualizar(CalcTO calcTO, int id){
+        int i=0;
+        try {
+            ps=conn.prepareStatement("UPDATE  calculadora SET num1 =?, num2 =?, operador =?, resultado =? WHERE id =?; ");
+            ps.setString(++i, calcTO.getNum1());
+            ps.setString(++i, calcTO.getNum2());
+            ps.setString(++i, String.valueOf(calcTO.getOperador()));
+            ps.setString(++i, calcTO.getResultado());
+            ps.setInt(++i, id);
+            ps.executeUpdate();
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void eliminar(int id){
+        try {
+            ps=conn.prepareStatement("DELETE FROM calculadora WHERE id=?");
+            ps.setInt(1, id);
             ps.executeUpdate();
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
-
-
 }
